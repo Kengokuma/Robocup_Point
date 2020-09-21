82b055dfc114d68314800b1af564a8c435c256b6
Kengokuma <kengo171227@gmail.com>
2020-09-21 17:31:23 +0900

    master

---
+++ script.js
@@ -0,0 +1,7 @@
+function myfunc() {
+        
+    var index = document.getElementById("tile").selectedIndex;
+    var value = document.getElementById("tile").value;
+    
+    alert("You have selected" + value+ ", with index "+index);
+}
